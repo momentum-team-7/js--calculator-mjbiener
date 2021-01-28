4d5255ec198eda0e11bcc9b819851d1a88c6315d
mjbiener <michaelbiener@yahoo.com>
2021-01-28 07:50:51 -0500

    initial attempt

---
+++ main.js
@@ -0,0 +1,9 @@
+// let buttons = document.querySelectorAll('button')
+// console.log(buttons)
+
+// for (let button of buttons) {
+//     button.addEventListener('click', event => {
+//         console.log(event.target.id)
+//         event.target.classList.add('highlight')    
+//     })
+// }
